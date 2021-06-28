3f79b6a653f395eea70366d8fe205d87ba6582bd
caothaihoanglong <81396158+caothaihoanglong@users.noreply.github.com>
2021-06-28 09:39:50 +0700

    Update mybtl.ino

--- mybtl.ino
+++ mybtl.ino
@@ -1,9 +1,10 @@
-#include <WiFi.h>
+include <WiFi.h>
 #include <BlynkSimpleEsp32.h> 
 #include <WiFiClient.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <DHT.h>
+unsigned long time1;
 int value,real_value;
 const int DHTPIN=19;                                      //Khởi tạo chân 19 cho cảm biến độ ẩm, nhiệt độ
 const int DHTTYPE=DHT11;
@@ -27,6 +28,7 @@ BLYNK_WRITE(V0)
 }
 DHT dht(DHTPIN, DHTTYPE);
 void setup(){
+  time1=millis();
   Serial.begin(9600);
   lcd.init();                                          
   lcd.backlight();                                       //Bật đèn nền lcd
@@ -40,29 +42,31 @@ void setup(){
   digitalWrite(32,LOW);                                  //Co chân 32 ở mức điện áp thấp
 }
 void loop(){
-  Blynk.run();                                           //Chạy blynk
-  for(int i=0;i<=9;i++){                                 //Dùng hàm for để đọc 10 lần giá trị cảm biến, sau đó lấy giá trị trung bình để được giá trị chính xác nhất.
+  if((unsigned long) (millis()-time1)>1000){
+    Blynk.run();                                           //Chạy blynk
+    for(int i=0;i<=9;i++){                                 //Dùng hàm for để đọc 10 lần giá trị cảm biến, sau đó lấy giá trị trung bình để được giá trị chính xác nhất.
     real_value+=analogRead(34);
-  }                                               
-  value=real_value/10;
-  int percent = map(value, 4095, 1860, 0, 100);          // Set giá thang giá trị đầu và giá trị cuối để đưa giá trị về thang từ 0-100. Giá trị thực đo được là 4095-1850. 
-  real_value=0;                                          //Đặt lại giá trị tổng
-   if(percent<45){                                       //Nếu giá trị độ ẩm dưới 45% thì bật bơm, ko thì tắt bơm
+    }                                               
+    value=real_value/10;
+    int percent = map(value, 4095, 1860, 0, 100);          // Set giá thang giá trị đầu và giá trị cuối để đưa giá trị về thang từ 0-100. Giá trị thực đo được là 4095-1850. 
+    real_value=0;                                          //Đặt lại giá trị tổng
+    if(percent<45){                                       //Nếu giá trị độ ẩm dưới 45% thì bật bơm, ko thì tắt bơm
     digitalWrite(33,HIGH);
+    }
+    else{
+      digitalWrite(33,LOW);
+    }
+    int h1=dht.readHumidity();                             //Gán h1 bằng độ ẩm đo được
+    int t1=dht.readTemperature();                          //Gán t1 bằng nhiệt độ đo được
+    Blynk.virtualWrite(V5,h1);                             //Gán h1 bằng V5 trên blynk
+    Blynk.virtualWrite(V6,t1);                             //Gán t1 bằng V6 trên blynk
+    Blynk.virtualWrite(V2,percent);                        //Gán percent bằng V2 trên blynk
+    lcd.setCursor(9,0);
+    lcd.print(t1);
+    lcd.print("doC");
+    lcd.setCursor(9,1);
+    lcd.print(h1);
+    lcd.print("%");
+    time1=millis();
   }
-  else{
-    digitalWrite(33,LOW);
-  }
-  int h1=dht.readHumidity();                             //Gán h1 bằng độ ẩm đo được
-  int t1=dht.readTemperature();                          //Gán t1 bằng nhiệt độ đo được
-  Blynk.virtualWrite(V5,h1);                             //Gán h1 bằng V5 trên blynk
-  Blynk.virtualWrite(V6,t1);                             //Gán t1 bằng V6 trên blynk
-  Blynk.virtualWrite(V2,percent);                        //Gán percent bằng V2 trên blynk
-  lcd.setCursor(9,0);
-  lcd.print(t1);
-  lcd.print("doC");
-  lcd.setCursor(9,1);
-  lcd.print(h1);
-  lcd.print("%");
-  delay(1000);
 }
